58aace54fc2661afbca4149550386ee496221861
Benjamin David Roberts <rc0029813@student.riversidecollege.ac.uk>
2018-09-11 10:41:15 +0100

    I have made it so that now when you click the move button the image moves up.

--- Move Picture/Form1.cs
+++ Move Picture/Form1.cs
@@ -19,7 +19,7 @@ namespace Move_Picture
 
         private void button1_Click(object sender, EventArgs e)
         {
-            pictureBox1.Location = new Point(pictureBox1.Location.X - 50, pictureBox1.Location.Y);
+            pictureBox1.Location = new Point(pictureBox1.Location.X, pictureBox1.Location.Y-50);
         }
     }
 }
