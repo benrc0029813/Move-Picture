327fa9095514417206cd088122bcfa15a7c351b2
Benjamin David Roberts <rc0029813@student.riversidecollege.ac.uk>
2018-09-11 10:56:59 +0100

    I have added notes to my code.

--- Move Picture/Form1.cs
+++ Move Picture/Form1.cs
@@ -16,21 +16,22 @@ namespace Move_Picture
         {
             InitializeComponent();
         }
-
+        //moves the picutre right
         private void button1_Click(object sender, EventArgs e)
         {
             pictureBox1.Location = new Point(pictureBox1.Location.X+50, pictureBox1.Location.Y);
         }
+        //moves the picture left
         private void button2_Click(object sender, EventArgs e)
         {
             pictureBox1.Location = new Point(pictureBox1.Location.X - 50, pictureBox1.Location.Y);
         }
-
+        //move the picture up
         private void button3_Click(object sender, EventArgs e)
         {
             pictureBox1.Location = new Point(pictureBox1.Location.X, pictureBox1.Location.Y - 50);
         }
-
+        //moves the picutre down
         private void button4_Click(object sender, EventArgs e)
         {
             pictureBox1.Location = new Point(pictureBox1.Location.X, pictureBox1.Location.Y + 50);
