ac867c9ed698e77e5a3ec54323a06acb4f059283
Benjamin David Roberts <rc0029813@student.riversidecollege.ac.uk>
2018-09-11 10:30:05 +0100

    I have uploaded an image of a star in addition to adding a button.

--- Move Picture/Form1.cs
+++ Move Picture/Form1.cs
@@ -16,5 +16,10 @@ namespace Move_Picture
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+        }
     }
 }
