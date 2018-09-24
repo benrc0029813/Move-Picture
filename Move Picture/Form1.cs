d051be14611e3b70928ab7bdcb0b4d3b3475f0a1
Benjamin David Roberts <rc0029813@student.riversidecollege.ac.uk>
2018-09-24 15:40:29 +0100

    I have added a new textbox and button and made it so that when the button is pressed it move the picture whatever distance that was typed into the textbox.

--- Move Picture/Form1.cs
+++ Move Picture/Form1.cs
@@ -56,5 +56,14 @@ namespace Move_Picture
             pictureBox1.Location = new Point(pictureBox1.Location.X - 50, pictureBox1.Location.Y);
             await Task.Delay(500);
         }
+
+        private async void button6_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 10; i++) 
+            {
+                pictureBox1.Location = new Point(pictureBox1.Location.X + 5, pictureBox1.Location.Y);
+                await Task.Delay(Convert.ToInt32(textBox1.Text));
+            }
+        }
     }
 }
